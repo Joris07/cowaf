569a5ec64bf224e771682f7793a51820e0e1b83c
Gourdon Joris <jo.070101@gmail.com>
2024-02-22 23:54:10 +0100

    Début intégration Front

---
+++ src/components/BackButton.vue
@@ -0,0 +1,36 @@
+<!-- BackButton.vue -->
+<template>
+    <div class="back-button" @click="goBack">
+        <svg xmlns="http://www.w3.org/2000/svg" width="9" height="14" viewBox="0 0 9 14" fill="none">
+            <path d="M7 2L2 7L7 12" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      goBack() {
+        window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+    .back-button {
+        position: fixed;
+        top: 66px;
+        left: 25px;
+        width: 38px;
+        height: 38px;
+        background-color: var(--color-blue);
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #fff;
+        cursor: pointer;
+    }
+  </style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <!-- Home.vue -->
 <template>
 	<div>
+		<BackButton />
 		<h1>Bienvenue sur ma page d'accueil Vue.js</h1>
+		<button class="button-border-blue bold">Valider</button>
 	</div>
 </template>
 
 <script>
+	import BackButton from '@/components/BackButton.vue';
+
 	export default {
 		name: 'Home',
+		components: { BackButton }
 	};
 </script>
 
