656b9d649271100d886a812c7b940c0c0e7dda0f
619yassine <yassinekhattabi@outlook.fr>
2024-03-11 19:09:34 +0100

    add bougies

--- src/views/Bougies.vue
+++ src/views/Bougies.vue
@@ -1,10 +1,24 @@
 <template>
 	<div id="main">
 		<div id="main-second">
-			<div class="vingtdeux">
+			<div>
 				<h1 class="bold">Combien de bougies a-t-il soufflé ?</h1>
+			</div>
+			<div class="vingtdeux">
 				<div id="container-photo">
 					<img src="/img/gateau.png" alt="">
+				</div>
+				<div id="number-selector" class="number-selector">
+					<div class="numbers-scroll-container">
+						<div 
+							v-for="num in 30" 
+							:key="num" 
+							class="number" 
+							:class="{ 'active': num === age }" 
+							@click="updateValue(num)">
+						{{ num }}
+						</div>
+					</div>
 				</div>
 			</div>
 			
@@ -17,7 +31,8 @@
 					<div id="texte">
 						<span id="nom-permis" v-if="!nomAnimal"></span>
 						<div id="nom-animal" v-if="nomAnimal">{{ nomAnimal }}</div>
-						<span id="age-permis"></span>
+						<span id="age-permis" v-if="!age"></span>
+						<div id="age-animal" v-if="age">{{ age }} ans</div>
 						<div id="description-permis">
 							<span class="description"></span>
 							<span class="description"></span>
@@ -39,11 +54,16 @@
 			return {
 				nomAnimal: '',
 				selectedImageUrl: null,
+				age: 0,
 			};
 		},
 		methods: {
 			nextStep(){
+				localStorage.setItem('age', this.age);
 				this.$router.push('/TraitAnimal');
+			},
+			updateValue(value) {
+				this.age = value;
 			}
 		},
 		created() {
@@ -68,13 +88,15 @@
 		flex-direction: column;
 		align-items: center;
 		gap: 22px;
+		width: 100%;
+		overflow: hidden;
 	}
 
 	#main-second {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
 		gap: 22px;
+		width: 100%;
 	}
 
 	h1 {
@@ -124,6 +146,14 @@
 		font-size: unset;
 	}
 
+	#age-animal {
+		width: 100%;
+		font-weight: 700;
+		border : none;
+		outline: none;
+		font-size: unset;
+	}
+
 	#age-permis {
 		width: 50px;
 		height: 10px;
@@ -143,5 +173,42 @@
 		flex-direction: column;
 		gap: 10px;
 	}
+	/**/
+	.number-selector {
+		overflow-x: auto;
+		display: flex;
+		width: 100%;
+		flex-wrap: nowrap;
+		padding-left: 47.5%;
+		padding-right: 42.5%;
+		position: relative;
+		scrollbar-width: none;
+	}
+
+	.numbers-scroll-container {
+		display: flex;
+		flex-wrap: nowrap;
+		/* position: relative; Pour le contexte de positionnement des enfants sticky */
+  		height: 100%;
+	}
+
+	.number {
+		flex: none; /* Éléments flexibles qui ne grandissent ni ne rétrécissent */
+		scroll-snap-align: center; /* Aligner correctement lors du défilement */
+		display: inline-block;
+		margin-right: 10px; /* Espacer les chiffres */
+		padding: 5px 10px; /* Couleur de fond pour les chiffres non-sélectionnés */
+		color: #09458E; /* Couleur du texte pour tous les chiffres */
+		text-align: center;
+		border-radius: 25%;
+		opacity: 1; /* Assurez-vous que les numéros sont pleinement opaques au départ */
+  		transition: opacity 0.3s;
+	}
+
+	.number.active {
+		background-color: #09458E; /* Couleur de fond pour le chiffre sélectionné */
+		color: white; /* Couleur du texte pour le chiffre sélectionné */
+	}
+
 </style>
   
