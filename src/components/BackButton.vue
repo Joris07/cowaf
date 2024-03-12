8c7082bb7affe87ab040f08759fead17ff3aa91a
Gourdon Joris <jo.070101@gmail.com>
2024-03-12 01:05:23 +0100

    Mise en place étapes avec KeepAlive

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,17 +1,15 @@
-<!-- BackButton.vue -->
 <template>
-	<div :style="buttonStyle" @click="goBack">
-		<svg xmlns="http://www.w3.org/2000/svg" width="9" height="14" viewBox="0 0 9 14" fill="none">
-			<path :stroke="arrowColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" d="M7 2L2 7L7 12" />
-		</svg>
+	<div :style="buttonStyle" @click="handleClick">
+	  <svg xmlns="http://www.w3.org/2000/svg" width="9" height="14" viewBox="0 0 9 14" fill="none">
+		<path :stroke="arrowColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" d="M7 2L2 7L7 12" />
+	  </svg>
 	</div>
-</template>
+  </template>
   
-<script>
-
-import router from '@/router';
-
-export default {
+  <script>
+  import router from '@/router';
+  
+  export default {
 	props: {
 		backgroundColor: {
 			type: String,
@@ -21,29 +19,36 @@ export default {
 			type: String,
 			default: 'white',
 		},
+		disableDefaultBehavior: {
+			type: Boolean,
+			default: false,
+		},
 	},
 	methods: {
-		goBack() {
-			window.history.length > 1 ? router.go(-1) : router.push('/');
+		handleClick() {
+			if (!this.disableDefaultBehavior) {
+				window.history.length > 1 ? router.go(-1) : router.push('/');
+			}
 		},
 	},
 	computed: {
 		buttonStyle() {
-		return {
-			position: 'absolute',
-			top: '30px',
-			left: '25px',
-			width: '30px',
-			height: '30px',
-			backgroundColor: this.backgroundColor,
-			borderRadius: '50%',
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			color: this.arrowColor,
-			cursor: 'pointer',
-		};
+			return {
+				position: 'absolute',
+				top: '30px',
+				left: '25px',
+				width: '30px',
+				height: '30px',
+				backgroundColor: this.backgroundColor,
+				borderRadius: '50%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				color: this.arrowColor,
+				cursor: 'pointer',
+			};
 		},
 	},
-};
-</script>
+  };
+  </script>
+  
